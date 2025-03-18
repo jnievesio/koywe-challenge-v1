26310f1fa044171fbd997a8923ca91bba3001837
joselyne.nieves <joselyne.nievesr@gmail.com>
2025-03-18 12:08:28 -0300

    feat(main):add configuration
     -configure the route, cors and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const cors = {
+    origin: configService.get<string>('CORS_ORIGINS').split('|'),
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  };
+
+  app.use(helmet());
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(cors);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Koywe challenge V1')
+    .setDescription('Endpoints para Koywe Challenge')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
