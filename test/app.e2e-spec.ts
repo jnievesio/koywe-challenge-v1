4ac7078d9dd9443eecd04a1c355511c24eebd9e3
joselyne.nieves <joselyne.nievesr@gmail.com>
2025-03-18 12:08:50 -0300

    test(app.e2e-spec):add e3e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,52 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { faker } from '@faker-js/faker';
+import { checkConnection, closeTestDB, configTestDB } from './utils/test-db';
 
-describe('AppController (e2e)', () => {
+describe('Test Apis de Koywe (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(async () => {
+    const testDB = await configTestDB();
+    app = testDB.app;
+  }, 30000);
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+  beforeEach(() => {
+    checkConnection();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Autentitación', () => {
+    it('Registrar un nuevo usuario', () => {
+      return request(app.getHttpServer())
+        .post('/api/v1/auth/register')
+        .send({
+          name: faker.person.fullName(),
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+        })
+        .expect(201);
+    });
+  });
+
+  describe('Usuarios', () => {
+    it('Listar usuarios registrados', () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/users')
+        .expect(200)
+        .expect((response) => {
+          expect(Array.isArray(response.body)).toBe(true);
+          expect(response.body[0]).toEqual(
+            expect.objectContaining({
+              id: expect.any(String),
+              name: expect.any(String),
+              email: expect.any(String),
+            }),
+          );
+        });
+    });
+  });
+
+  afterAll(async () => {
+    await closeTestDB();
   });
 });
